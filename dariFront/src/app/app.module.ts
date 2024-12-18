2ffa3f8f034a96512bbd20741e5fa9478cd395cc
Ibrahim Ellouze <ellouzeibrahim@isimsf.u-sfax.tn>
2024-12-18 12:29:12 +0100

    quality of life

--- dariFront/src/app/app.module.ts
+++ dariFront/src/app/app.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field'; // Import form field module
-import { MatSelectModule } from '@angular/material/select'; // Import select module
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core'; 
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { IndexComponent } from './components/index/index.component';
 import { HomeComponent } from './components/home/home.component';
 import { RoomComponent } from './components/room/room.component';
-import { DeviceComponent } from './components/devices/devices.component';  // Import HomeComponent
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
-//import { RoomSelectingDialogComponent } from './components/room-selecting-dialog/room-selecting-dialog.component';
+import { DeviceComponent } from './components/devices/devices.component';
+
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'index', component: IndexComponent },
@@ -35,25 +35,21 @@ const routes: Routes = [
     HomeComponent, 
     RoomComponent, 
     DeviceComponent,
-    //RoomSelectingDialogComponent
-    //DevicesComponent, RoomSelectingDialogComponent,
-     // Add HomeComponent here
   ],
   imports: [
-
     ReactiveFormsModule,
     MatFormFieldModule,
     MatSelectModule,
     MatOptionModule,
     BrowserModule,
     FormsModule,
-    HttpClientModule,
     RouterModule.forRoot(routes),
     MatDialogModule, 
     MatSnackBarModule,
-
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(withFetch())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
