58ec3be33d7d89e2bfc1907043fa446db2534f90
Ibrahim Ellouze <ellouzeibrahim@isimsf.u-sfax.tn>
2024-12-20 09:12:45 +0100

    devops docker

--- dari/src/main/java/com/example/dari/entities/Device.java
+++ dari/src/main/java/com/example/dari/entities/Device.java
@@ -1,28 +1,13 @@
 package com.example.dari.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
 
 import java.beans.Transient;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-import jakarta.persistence.*;
-
-import com.example.dari.entities.Room;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
-@Getter 
-@Setter
 public class Device {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,6 +27,19 @@ public class Device {
 
     private LocalDateTime turnedOnAt; // Time when the device was turned ON
 
+    public Device(Long id, String name, boolean isConnected, boolean isActive, Double energyRate, Room room, LocalDateTime turnedOnAt) {
+        this.id = id;
+        this.name = name;
+        this.isConnected = isConnected;
+        this.isActive = isActive;
+        this.energyRate = energyRate;
+        this.room = room;
+        this.turnedOnAt = turnedOnAt;
+    }
+
+    public Device() {
+    }
+
     @Transient
     public Double getEnergyConsumption() {
         if (isActive && turnedOnAt != null) {
@@ -50,5 +48,114 @@ public class Device {
             return minutesOn * energyRate;
         }
         return 0.0;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Device)) return false;
+        final Device other = (Device) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$id = this.getId();
+        final Object other$id = other.getId();
+        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
+        final Object this$name = this.getName();
+        final Object other$name = other.getName();
+        if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
+        if (this.isConnected() != other.isConnected()) return false;
+        if (this.isActive() != other.isActive()) return false;
+        final Object this$energyRate = this.getEnergyRate();
+        final Object other$energyRate = other.getEnergyRate();
+        if (this$energyRate == null ? other$energyRate != null : !this$energyRate.equals(other$energyRate))
+            return false;
+        final Object this$room = this.getRoom();
+        final Object other$room = other.getRoom();
+        if (this$room == null ? other$room != null : !this$room.equals(other$room)) return false;
+        final Object this$turnedOnAt = this.getTurnedOnAt();
+        final Object other$turnedOnAt = other.getTurnedOnAt();
+        if (this$turnedOnAt == null ? other$turnedOnAt != null : !this$turnedOnAt.equals(other$turnedOnAt))
+            return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Device;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $id = this.getId();
+        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
+        final Object $name = this.getName();
+        result = result * PRIME + ($name == null ? 43 : $name.hashCode());
+        result = result * PRIME + (this.isConnected() ? 79 : 97);
+        result = result * PRIME + (this.isActive() ? 79 : 97);
+        final Object $energyRate = this.getEnergyRate();
+        result = result * PRIME + ($energyRate == null ? 43 : $energyRate.hashCode());
+        final Object $room = this.getRoom();
+        result = result * PRIME + ($room == null ? 43 : $room.hashCode());
+        final Object $turnedOnAt = this.getTurnedOnAt();
+        result = result * PRIME + ($turnedOnAt == null ? 43 : $turnedOnAt.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "Device(id=" + this.getId() + ", name=" + this.getName() + ", isConnected=" + this.isConnected() + ", isActive=" + this.isActive() + ", energyRate=" + this.getEnergyRate() + ", room=" + this.getRoom() + ", turnedOnAt=" + this.getTurnedOnAt() + ")";
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public boolean isConnected() {
+        return this.isConnected;
+    }
+
+    public boolean isActive() {
+        return this.isActive;
+    }
+
+    public Double getEnergyRate() {
+        return this.energyRate;
+    }
+
+    public Room getRoom() {
+        return this.room;
+    }
+
+    public LocalDateTime getTurnedOnAt() {
+        return this.turnedOnAt;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setConnected(boolean isConnected) {
+        this.isConnected = isConnected;
+    }
+
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public void setEnergyRate(Double energyRate) {
+        this.energyRate = energyRate;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public void setTurnedOnAt(LocalDateTime turnedOnAt) {
+        this.turnedOnAt = turnedOnAt;
     }
 }
--- dariFront/src/app/app-routing.module.ts
+++ dariFront/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { IndexComponent } from './components/index/index.component';
+import { HomeComponent } from './components/home/home.component';
+import { RoomComponent } from './components/room/room.component';
+import { DeviceComponent } from './components/devices/devices.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { StoreComponent } from './components/store/store.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'index', component: IndexComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'homes/:userId/rooms', component: HomeComponent },
+  { path: 'homes/:userId/rooms/:roomId/devices', component: RoomComponent },
+  { path: 'users/:userId/devices/connected', component: DeviceComponent },
+  { path: 'users/:userId/dashboard', component: DashboardComponent },
+  { path: 'users/:userId/store', component: StoreComponent },
+  { path: '', redirectTo: '/index', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
